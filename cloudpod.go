07715945ebc95e23dfaa7420d0289857716feed9
Taariq Mullins <tjma2001@gmail.com>
2018-03-23 12:58:09 +0200

    refactor(cloudpod): updated cloudpod, it now deletes specified container

--- cloudpod.go
+++ cloudpod.go
@@ -1,24 +1,8 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -38,29 +22,27 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("spinnaker").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		fmt.Printf("pods", pods.Items)
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("spinnaker").Get("spin-clouddriver-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod not found\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod\n")
-		}
-
-		time.Sleep(10 * time.Second)
+	pods, err := clientset.CoreV1().Pods("spinnaker").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
-}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	for _, element := range pods.Items {
+		if strings.Contains(element.ObjectMeta.Name, "spin-clouddriver") {
+			fmt.Printf("Found the desired clouddriver pod\n")
+			fmt.Printf(element.ObjectMeta.Name)
+			err = clientset.CoreV1().Pods("spinnaker").Delete(element.ObjectMeta.Name, &metav1.DeleteOptions{})
+			if errors.IsNotFound(err) {
+				fmt.Printf("\nPod not found\n")
+			} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+				fmt.Printf("\nError deleting pod %v\n", statusError.ErrStatus.Message)
+			} else if err != nil {
+				panic(err.Error())
+			} else {
+				fmt.Printf("\nDeleted pod\n")
+			}
+		}
+	}
 
+	time.Sleep(36 * time.Hour)
+}
